ed134d6e10f8359c9914dd7c3209fb718e901779
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-09-12 17:29:20 +0200

    refactor tasks view

--- src/views/tasksView.js
+++ src/views/tasksView.js
@@ -1,15 +1,38 @@
+import Controller from '../controller';
+
 const TasksView = (() => {
-  const renderTasks = (project) => {
-    const list = document.createElement('ul');
-    project.tasks.forEach((task) => {
-      list.innerHTML += `
-        <li>${task.title}</li>
+  const tasks = Controller.getActiveProjectTasks();
+  const tasksCollection = document.getElementById('tasks');
+
+  const render = () => {
+    tasks.forEach((task) => {
+      const taskItem = document.createElement('div');
+      taskItem.innerHTML = `
+        <h1>${task.title}</h1>
+        <p>${task.description}</p>
+        <p>${task.dueDate}</p>
+        <p>${task.priority}</p>
+        <p>${task.taskDone}</p>
       `;
+      tasksCollection.append(taskItem);
     });
-    return list;
   };
 
-  return { renderTasks };
+  const init = () => {
+    render();
+  };
+
+  // const renderTasks = (project) => {
+  //   const list = document.createElement('ul');
+  //   project.tasks.forEach((task) => {
+  //     list.innerHTML += `
+  //       <li>${task.title}</li>
+  //     `;
+  //   });
+  //   return list;
+  // };
+
+  return { init, render };
 })();
 
 export default TasksView;
