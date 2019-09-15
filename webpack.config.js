fcb83e84c4cb5f5e442a2a34075476af03bf7268
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-09-15 22:32:44 +0200

    add source-map-loader for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
+const exclude = [path.resolve(__dirname, 'dist')];
+console.log(exclude);
+
 module.exports = {
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'source-map-loader',
+      exclude
+    }]
+  },
+  devServer: {
+    contentBase: 'dist/'
   }
 };
