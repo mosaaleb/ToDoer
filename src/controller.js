d05700f1db5eae3ec809db165be90f5001449933
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-01 16:22:22 +0200

    added removeProject and removeTask methods

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,7 @@
 import Model from './model';
 import Project from './project';
-// import UI from './views/ui';
-import TasksView from './views/tasksView';
-import ProjectsView from './views/projectsView';
-
+// import ProjectsView from './views/projectsView';
+// import TasksView from './views/tasksView';
 
 const Controller = (() => {
   const setActiveProject = (project) => {
@@ -16,58 +14,55 @@ const Controller = (() => {
 
   const getActiveProjectTasks = () => getActiveProject().tasks;
 
-  // HANDLERS
-  const projectSelectHandler = (projectIndex) => {
-    const clickedProject = getProjects()[projectIndex];
-    setActiveProject(clickedProject);
-    TasksView.render(getActiveProjectTasks());
-  };
-
-  const addProjectHandler = (projectName, projectColor) => {
+  const addProject = (projectName, projectColor) => {
     const project = Project(projectName, projectColor);
     Model.addProject(project);
   };
 
-  const addTaskHandler = (taskTitle, taskDesc, taskDue) => {
+  const addTask = (taskTitle, taskDesc, taskDue) => {
     const task = {
       title: taskTitle,
       description: taskDesc,
       dueDate: taskDue
     };
     const activeProject = getActiveProject();
-    const updatedProject = Project(activeProject.name, activeProject.color, activeProject.tasks);
     const allProjects = Model.getAll();
 
-    updatedProject.tasks.push(task);
-    const index = allProjects.findIndex((project) => project.name === updatedProject.name);
-    allProjects.splice(index, 1, updatedProject);
+    activeProject.tasks.push(task);
+    const index = allProjects.findIndex((project) => project.name === activeProject.name);
+    allProjects.splice(index, 1, activeProject);
     Model.add(allProjects);
-    // TasksView.render(getActiveProjectTasks());
   };
 
   const init = () => {
     setActiveProject(getProjects()[0]);
-
-    ProjectsView.init(getProjects());
-    TasksView.init(getActiveProjectTasks());
-
-    // BIND
-    ProjectsView.bindProjectSelect(projectSelectHandler);
-    ProjectsView.bindAddProject(addProjectHandler);
-    TasksView.bindAddTask(addTaskHandler);
-
-    // ATTACH EVENTS
-    Model.addProjectEvent.addObserver(ProjectsView.render);
-    Model.addTaskEvent.addObserver(TasksView.render);
   };
 
+  const removeProject = (index) => {
+    const projects = Model.getAll();
+    projects.splice(index, 1);
+    Model.add(projects);
+  };
+
+  const removeTask = (index) => {
+    const projects = Model.getAll();
+    const activeProject = getActiveProject();
+    const activeProjectIndex = projects.findIndex((project) => project.name === activeProject.name);
+    activeProject.tasks.splice(index, 1);
+    projects.splice(activeProjectIndex, 1, activeProject);
+    Model.add(projects);
+  };
 
   return {
     init,
     setActiveProject,
     getActiveProject,
     getActiveProjectTasks,
-    getProjects
+    getProjects,
+    addProject,
+    addTask,
+    removeProject,
+    removeTask
   };
 })();
 
