cb335d28acc143b0fa836aac11f9f75e12652a9d
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-07 15:17:21 +0200

    Why:
    * refactor projectsView to the use of handlebars templates
    
    This change addresses the need by:
    
    * adding handlebars templating

--- src/views/projectsView.js
+++ src/views/projectsView.js
@@ -4,72 +4,45 @@ import projectsTemplate from '../index.hbs';
 
 const ProjectsView = (() => {
   const navBar = document.getElementById('projects');
-  const navList = document.createElement('ul');
-  const form = document.createElement('form');
+
+  const renderForm = () => {
+    const form = document.getElementById('project-form');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const projectNameInput = document.querySelector('#project-name-input');
+      const projectColorInput = document.querySelector('#project-color-input');
+
+      const projectName = projectNameInput.value;
+      const projectColor = projectColorInput.value;
+      projectNameInput.value = '';
+      projectColorInput.value = '#ffffff';
+      Controller.addProject(projectName, projectColor);
+      Model.addProjectEvent.notify(Controller.getProjects());
+    });
+  };
 
   const update = (projects) => {
     navBar.innerHTML = projectsTemplate(projects);
-  };
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const projectNameInput = document.querySelector('.project-name-input');
-    const projectColorInput = document.querySelector('.project-color-input');
-
-    const projectName = projectNameInput.value;
-    const projectColor = projectColorInput.value;
-    projectNameInput.value = '';
-    projectColorInput.value = '#ffffff';
-    Controller.addProject(projectName, projectColor);
-    Model.addProjectEvent.notify(Controller.getProjects());
-
-    // const projects = Controller.getProjects();
-    // Controller.setActiveProject(projects[projects.length - 1]);
-    // Model.projectSelectEvent.notify(Controller.getActiveProjectTasks());
-  });
-
-  // const update = (projects) => {
-  //   navList.innerHTML = '';
-  //   projects.forEach((project, index) => {
-  //     const projectItem = document.createElement('div');
-  //     const projectName = document.createElement('div');
-  //     const deleteButton = document.createElement('button');
-  //     deleteButton.id = index;
-  //     deleteButton.textContent = 'X';
-  //     projectName.textContent = project.name;
-  //     projectItem.appendChild(projectName);
-  //     projectItem.appendChild(deleteButton);
-  //     projectName.addEventListener('click', () => {
-  //       if (project.name !== Controller.getActiveProject().name) {
-  //         Controller.setActiveProject(project);
-  //         Model.projectSelectEvent.notify(Controller.getActiveProjectTasks());
-  //       }
-  //     });
-  //     deleteButton.addEventListener('click', (event) => {
-  //       Controller.removeProject(event.target.id);
-  //       Model.removeProjectEvent.notify(Controller.getProjects());
-  //     });
-  //     navList.appendChild(projectItem);
-  //   });
-  // };
-
-  const renderForm = () => {
-    const input = document.createElement('input');
-    const colorInput = document.createElement('input');
-    const submitButton = document.createElement('button');
-    input.type = 'text';
-    colorInput.type = 'color';
-    input.className = 'project-name-input';
-    colorInput.className = 'project-color-input';
-    submitButton.textContent = 'Add Project';
-    form.append(input, colorInput, submitButton);
-    navBar.appendChild(form);
+    const deleteButton = document.getElementsByClassName('delete-project-button');
+    const projectItems = document.getElementsByClassName('project-name');
+    [...deleteButton].forEach((button) => {
+      button.addEventListener('click', (event) => {
+        Controller.removeProject(event.target.parentElement.id);
+        Model.removeProjectEvent.notify(Controller.getProjects());
+      });
+    });
+    [...projectItems].forEach((project) => {
+      project.addEventListener('click', (event) => {
+        const allProjects = Controller.getProjects();
+        Controller.setActiveProject(allProjects[event.target.parentElement.id]);
+        Model.projectSelectEvent.notify(Controller.getActiveProjectTasks());
+      });
+    });
+    renderForm();
   };
 
   const init = (projects) => {
-    navBar.appendChild(navList);
     update(projects);
-    renderForm();
   };
 
   return {
