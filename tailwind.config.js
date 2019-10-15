73ba28b21afefede92e23351c30a46b711f2de9f
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-15 23:33:01 +0200

    Add custom styles tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
         inner: 'inset -2px 0 10px 0 rgba(0, 0, 0, 0.06)',
       },
       colors: {
-        silverGray: '#c5d0eb'
+        silverGray: '#c5d0eb',
+        blue: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#284AF6',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
         // <!-- #DBE4FA #DBE4FA #58A3F4 #B3BFD7 #284AF6 #2637AA #E7EBF7-->
       }
     }
