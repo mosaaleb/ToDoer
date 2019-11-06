1878242b88ff88533b1e06782ead2e8c5f0a8ee8
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-11-07 00:00:34 +0200

    Fix user current time

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// TODO: Refactor time functionality and add setinterval for current hours
+
 import '../images/logo.png';
 import '../images/add-icon.svg';
 import '../images/delete-icon.svg';
@@ -23,3 +25,26 @@ Model.addProjectEvent.addObserver(ProjectsView);
 Model.removeProjectEvent.addObserver(ProjectsView);
 Model.addTaskEvent.addObserver(TasksView);
 Model.removeTaskEvent.addObserver(TasksView);
+
+// Time functionaliity
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September ', 'October', 'November', 'December'];
+const formatTime = (date) => {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? 'pm' : 'am';
+  hours %= 12;
+  hours = hours || 12;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  const strTime = `${hours}:${minutes} ${ampm}`;
+  return strTime;
+};
+
+const date = new Date();
+const month = months[date.getMonth()];
+const weekDay = days[date.getDay()];
+const monthDay = date.getDate();
+const hour = formatTime(date);
+
+document.getElementById('date').textContent = `${monthDay} ${month}`;
+document.getElementById('day-time').textContent = `${weekDay} ${hour}`;
